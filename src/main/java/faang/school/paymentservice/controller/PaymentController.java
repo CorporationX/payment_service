7b285213f18e347e5dca005e24cee57ed811ca7f
DonMaxim <donmaxim1990@gmail.com>
2024-08-06 17:31:16 -0300

    Start doing tests

--- src/main/java/faang/school/paymentservice/controller/PaymentController.java
+++ src/main/java/faang/school/paymentservice/controller/PaymentController.java
@@ -1,17 +1,9 @@
 package faang.school.paymentservice.controller;
 
-import faang.school.paymentservice.dto.Currency;
 import faang.school.paymentservice.dto.PaymentRequest;
-
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.util.Random;
 import faang.school.paymentservice.dto.PaymentResponse;
-import faang.school.paymentservice.dto.PaymentStatus;
-import faang.school.paymentservice.service.exchange.ExchangeService;
+import faang.school.paymentservice.service.payment.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,36 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PaymentController {
-    private final ExchangeService exchangeService;
-    @Value("${app.currency.base}")
-    private String baseCurrency;
-    private static final String RESPONSE_WITHOUT_EXCHANGE = "Dear friend! Thank you for your purchase! " +
-            "Your payment on %s %s was accepted.";
-    private static final String RESPONSE_WITH_EXCHANGE = "Dear friend! Thank you for your purchase! " +
-            "Your payment on %s %s converted to %s %s and was accepted.";
+    private final PaymentService paymentService;
 
     @PostMapping("/payment")
-    public ResponseEntity<PaymentResponse> sendPayment(@RequestBody @Validated PaymentRequest dto) {
-        BigDecimal amount = dto.amount();
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        String formattedSum = decimalFormat.format(amount);
-        int verificationCode = new Random().nextInt(1000, 10000);
-        String message = String.format(RESPONSE_WITHOUT_EXCHANGE, formattedSum, dto.currency().name());
-
-        if (!exchangeService.isCurrencyBase(dto)) {
-            amount = exchangeService.getAmountInBaseCurrency(dto);
-            String formattedNewSum = decimalFormat.format(amount);
-            message = String.format(RESPONSE_WITH_EXCHANGE, formattedSum,
-                    dto.currency().name(), formattedNewSum, baseCurrency);
-        }
-
-        return ResponseEntity.ok(new PaymentResponse(
-                PaymentStatus.SUCCESS,
-                verificationCode,
-                dto.paymentNumber(),
-                amount,
-                Currency.valueOf(baseCurrency),
-                message)
-        );
+    public PaymentResponse sendPayment(@RequestBody @Validated PaymentRequest dto) {
+        return paymentService.processPayment(dto);
     }
 }
