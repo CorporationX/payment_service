3fdd368aac7ca0df9389fc767547f2703d77a5bc
shade2903 <nightshade2903@gmail.com>
2024-08-21 11:07:04 +0300

    feature-BJS2-15636
    Получение курсов валют относительно базовой валюты

--- src/main/java/faang/school/paymentservice/dto/client/ExchangeRatesResponse.java
+++ src/main/java/faang/school/paymentservice/dto/client/ExchangeRatesResponse.java
@@ -1,6 +1,8 @@
 package faang.school.paymentservice.dto.client;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,8 +12,11 @@ import java.util.Map;
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ExchangeRatesResponse {
+    @NotBlank
     private String base;
     private long timestamp;
+    @NotBlank
     private String date;
-    private Map<String,Double> rates;
+    @NotNull
+    private Map<String, Double> rates;
 }
