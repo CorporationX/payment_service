08255550a49d8ce84123ffd4e478251955641fc7
ifef228 <ifef228@gmail.com>
2024-09-30 13:33:09 +0300

    зависимость gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:2.7.0")
 
     /**
      * Utils & Logging
