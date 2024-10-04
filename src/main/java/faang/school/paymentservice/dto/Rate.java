7f2b9ec4391dcdf85cbaf201811dd0ac9dca55c3
ifef228 <ifef228@gmail.com>
2024-10-04 14:52:13 +0300

    убрал ненужные поля из Rate

--- src/main/java/faang/school/paymentservice/dto/Rate.java
+++ src/main/java/faang/school/paymentservice/dto/Rate.java
@@ -2,14 +2,10 @@ package faang.school.paymentservice.dto;
 
 import lombok.Data;
 
-import java.util.Date;
 import java.util.Map;
 
 @Data
 public class Rate {
-    private boolean success;
-    private long timestamp;
     private String base;
-    private Date date;
     private Map<String, Double> rates;
 }
