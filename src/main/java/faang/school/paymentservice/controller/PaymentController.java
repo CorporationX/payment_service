e15c71831b56f8881b4503a803e490c90fa7df57
Ivan <spvanym@gmail.com>
2024-10-06 23:22:29 +0300

    Сырая, наполовину сворованная и написанная с гпт дичь

--- src/main/java/faang/school/paymentservice/controller/PaymentController.java
+++ src/main/java/faang/school/paymentservice/controller/PaymentController.java
@@ -11,7 +11,6 @@ import faang.school.paymentservice.response.CurrencyExchangeResponse;
 import faang.school.paymentservice.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
