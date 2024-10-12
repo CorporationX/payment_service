5412b236f4c09c36b25bf9b538529b153b3bd71c
ifef228 <ifef228@gmail.com>
2024-10-12 17:46:04 +0300

    вынес бизнес логику в сервис

--- src/main/java/faang/school/paymentservice/controller/PaymentController.java
+++ src/main/java/faang/school/paymentservice/controller/PaymentController.java
@@ -1,9 +1,7 @@
 package faang.school.paymentservice.controller;
 
-import faang.school.paymentservice.dto.Currency;
 import faang.school.paymentservice.dto.PaymentRequest;
 import faang.school.paymentservice.dto.PaymentResponse;
-import faang.school.paymentservice.dto.PaymentStatus;
 import faang.school.paymentservice.service.ConvertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,9 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.DecimalFormat;
-import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,21 +22,6 @@ public class PaymentController {
 
     @PostMapping("/payment")
     public ResponseEntity<PaymentResponse> sendPayment(@RequestBody @Validated PaymentRequest dto) {
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        String formattedSum = decimalFormat.
-                format(convertService.convert(dto.currency(), Currency.valueOf(base), dto.amount()));
-        int verificationCode = new Random().nextInt(1000, 10000);
-        String message = String.format("Dear friend! Thank you for your purchase! " +
-                        "Your payment on %s %s was accepted.",
-                formattedSum, dto.currency().name());
-
-        return ResponseEntity.ok(new PaymentResponse(
-                PaymentStatus.SUCCESS,
-                verificationCode,
-                dto.paymentNumber(),
-                dto.amount(),
-                dto.currency(),
-                message)
-        );
+        return ResponseEntity.ok(convertService.getConvertedPayment(dto));
     }
 }
--- src/main/java/faang/school/paymentservice/service/ConvertService.java
+++ src/main/java/faang/school/paymentservice/service/ConvertService.java
@@ -1,6 +1,9 @@
 package faang.school.paymentservice.service;
 
 import faang.school.paymentservice.dto.Currency;
+import faang.school.paymentservice.dto.PaymentRequest;
+import faang.school.paymentservice.dto.PaymentResponse;
+import faang.school.paymentservice.dto.PaymentStatus;
 import faang.school.paymentservice.dto.Rate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +11,11 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +23,28 @@ public class ConvertService {
     private final RedisCacheManager manager;
     private final CurrencyService currencyService;
 
+    @Value("${exchange_rates.base}")
+    private String base;
+
     @Value("${spring.cache.redis.caches.current_rate}")
     private String rateCache;
+
+    public PaymentResponse getConvertedPayment(PaymentRequest dto) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String formattedSum = decimalFormat.
+                format(convert(dto.currency(), Currency.valueOf(base), dto.amount()));
+        int verificationCode = new Random().nextInt(1000, 10000);
+        String message = String.format("Dear friend! Thank you for your purchase! " +
+                        "Your payment on %s %s was accepted.",
+                formattedSum, dto.currency().name());
+        return new PaymentResponse(
+                PaymentStatus.SUCCESS,
+                verificationCode,
+                dto.paymentNumber(),
+                dto.amount(),
+                dto.currency(),
+                message);
+    }
 
     public BigDecimal convert(Currency convertable, Currency base, BigDecimal sum) {
         Rate rate = addCommission(convertable, base);
@@ -43,7 +68,7 @@ public class ConvertService {
             double baseCur = mapRate.get(convertable.getName());
             double convertableCur = mapRate.get(base1.getName());
 
-            newMapRate.put(base1.getName(), convertableCur/baseCur);
+            newMapRate.put(base1.getName(), convertableCur / baseCur);
         }
         rate.setRates(newMapRate);
 
