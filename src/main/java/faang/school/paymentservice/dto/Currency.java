51f2c295f1381f0328c76c7d87149d1a0d728398
ifef228 <ifef228@gmail.com>
2024-10-12 17:46:28 +0300

    форматирование

--- src/main/java/faang/school/paymentservice/dto/Currency.java
+++ src/main/java/faang/school/paymentservice/dto/Currency.java
@@ -3,9 +3,11 @@ package faang.school.paymentservice.dto;
 public enum Currency {
     USD("USD"), EUR("EUR");
     private final String curName;
+
     Currency(String cur) {
         curName = cur;
     }
+
     public String getName() {
         return curName;
     }
