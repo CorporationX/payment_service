f61d2de7c1702ab7f7e30ade0bee34af208e2f0b
Sultan <sultan.inked@gmail.com>
2024-10-29 11:36:23 +0500

    BJS2-40421 add user context

--- src/main/java/faang/school/paymentservice/publisher/redis/RedisEventPublisher.java
+++ src/main/java/faang/school/paymentservice/publisher/redis/RedisEventPublisher.java
@@ -4,7 +4,6 @@ import faang.school.paymentservice.dto.OperationMessage;
 import faang.school.paymentservice.publisher.EventPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,14 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("redis")
 public class RedisEventPublisher implements EventPublisher<OperationMessage> {
-    @Qualifier("redisTemplateWithJackson")
     private final RedisTemplate<String, Object> redisTemplate;
     private final String topic;
 
     @Autowired
-    public RedisEventPublisher(RedisTemplate<String, Object> redisTemplate,
+    public RedisEventPublisher(RedisTemplate<String, Object> redisTemplateWithJackson,
                                @Value("${spring.data.redis.channel.pending_operation}") String topic) {
-        this.redisTemplate = redisTemplate;
+        this.redisTemplate = redisTemplateWithJackson;
         this.topic = topic;
     }
 
