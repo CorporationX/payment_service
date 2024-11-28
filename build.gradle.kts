de1f1b3ef91bc3b6e7c663380677f490152f52b9
Sanasar1 <almasyansb@gmail.com>
2024-11-28 21:07:00 +0300

    feat(currencyexchange): добавил scheduler для получения курса обмена валют

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,12 @@ dependencies {
      * Spring boot starters
      */
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.retry:spring-retry:2.0.10")
 
     /**
      * Utils & Logging
---
+++ src/main/java/faang/school/paymentservice/scheduler/CurrencyRateFetcher.java
@@ -0,0 +1,18 @@
+package faang.school.paymentservice.scheduler;
+
+import faang.school.paymentservice.service.CurrencyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CurrencyRateFetcher {
+
+    private final CurrencyService currencyService;
+
+    @Scheduled(cron = "${scheduled.exchangerates.cron}")
+    public void fetchCurrencyRates() {
+        currencyService.getCurrentExchangeRates();
+    }
+}
