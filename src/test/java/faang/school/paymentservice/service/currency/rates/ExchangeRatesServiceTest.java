14d186c5087d86c4439f0d12e6d997b39ed2e030
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-12-06 14:37:45 +0300

    added recover-methods and fixed tests

--- src/test/java/faang/school/paymentservice/service/currency/rates/ExchangeRatesServiceTest.java
+++ src/test/java/faang/school/paymentservice/service/currency/rates/ExchangeRatesServiceTest.java
@@ -10,16 +10,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -31,20 +40,27 @@ public class ExchangeRatesServiceTest {
     @Mock
     private RedisTemplate<String, Double> redisTemplate;
 
+    private String mockCurrency;
+    private ObjectMapper objectMapper;
     private ExchangeRatesService exchangeRatesService;
-
-    public static MockWebServer mockWebServer;
+    private static MockWebServer mockWebServer;
 
     @BeforeEach
     void init() throws IOException {
+        mockCurrency = getMockedResponse().toString();
+        objectMapper = new ObjectMapper();
         mockWebServer = new MockWebServer();
         mockWebServer.start();
+
         String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort());
         WebClient.Builder webClientbuilder = WebClient.builder();
+
         webClient = webClientbuilder
                 .baseUrl(baseUrl)
                 .build();
+
         exchangeRatesService = new ExchangeRatesService(webClient, redisTemplate);
+        ReflectionTestUtils.setField(exchangeRatesService, "baseCurrency", Currency.EUR);
     }
 
     @AfterEach
@@ -54,17 +70,33 @@ public class ExchangeRatesServiceTest {
 
     @Test
     void testSuccessfulAttemptToGetExchangeRates() throws Exception {
-        String mockCurrency = getMockedResponse().toString();
-        ObjectMapper objectMapper = new ObjectMapper();
-        ReflectionTestUtils.setField(exchangeRatesService, "baseCurrency", Currency.EUR);
-
         mockWebServer.enqueue(new MockResponse()
                 .setBody(objectMapper.writeValueAsString(mockCurrency))
                 .addHeader("Content-Type", "application/json"));
 
-        String currencyMono = exchangeRatesService.getExchangeRates();
+        String result = exchangeRatesService.getExchangeRates();
 
-        assertEquals(mockCurrency, currencyMono.replace("\"", ""));
+        assertEquals(mockCurrency, result.replace("\"", ""));
+    }
+
+    @Test
+    public void testGetExchangeRatesWithWebClientResponseException() {
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(404)
+                .setBody("Not Found"));
+
+        StepVerifier.create(Mono.fromCallable(() -> exchangeRatesService.getExchangeRates()))
+                .expectError(WebClientResponseException.class)
+                .verify();
+    }
+
+    @Test
+    public void testGetExchangeRatesWithResourceAccessException() throws IOException {
+        mockWebServer.shutdown();
+
+        StepVerifier.create(Mono.fromCallable(() -> exchangeRatesService.getExchangeRates()))
+                .expectError(WebClientRequestException.class)
+                .verify();
     }
 
     private static HashMap<String, String> getMockedResponse() {
@@ -77,5 +109,9 @@ public class ExchangeRatesServiceTest {
         map.put("USD", "1.055342");
         map.put("EUR", "1.0");
         return map;
+    }
+
+    private String retryingGetExchangeRates() {
+        return exchangeRatesService.getExchangeRates();
     }
 }
