418d164cbc1d29da36176a4d2964028858d12651
Stanislav <stasikbygaev@gmail.com>
2025-01-21 14:31:11 +0500

    Добавил equalsAndHashcode в Auditing и BaseEntity

--- src/main/java/faang/school/paymentservice/entity/Auditing.java
+++ src/main/java/faang/school/paymentservice/entity/Auditing.java
@@ -3,6 +3,7 @@ package faang.school.paymentservice.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
+@EqualsAndHashCode
 public abstract class Auditing {
     @CreatedDate
     @Column(name = "created_at")
--- src/main/java/faang/school/paymentservice/entity/BaseEntity.java
+++ src/main/java/faang/school/paymentservice/entity/BaseEntity.java
@@ -4,10 +4,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @MappedSuperclass
 @Getter
+@EqualsAndHashCode(callSuper = true)
 public abstract class BaseEntity extends Auditing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
