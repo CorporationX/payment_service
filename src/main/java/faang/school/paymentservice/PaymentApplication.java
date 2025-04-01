d442a5d94f67a4bba6aff0b6b1115be8d3498168
Artem Doronin <doronintema2018@yandex.ru>
2025-04-01 14:09:13 +0500

    CurrencyService

--- src/main/java/faang/school/paymentservice/PaymentApplication.java
+++ src/main/java/faang/school/paymentservice/PaymentApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
-@RequiredArgsConstructor
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
