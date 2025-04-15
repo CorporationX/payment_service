1a30abe305e1321b7e12009e9e85acebdd5716ec
SergeyErmoshenko <ermoshenko.work@gmail.com>
2025-04-15 14:13:17 +0300

    вынес ошибки

--- src/main/java/faang/school/paymentservice/messages/ErrorMessages.java
+++ src/main/java/faang/school/paymentservice/messages/ErrorMessages.java
@@ -2,4 +2,7 @@ package faang.school.paymentservice.messages;
 
 public class ErrorMessages {
     public static final String FAILED_TO_ACKNOWLEDGE_KAFKA_MESSAGE = "Failed to acknowledge Kafka message";
+    public static final String ERROR_DESERIALIZING = "Error deserializing ";
+    public static final String SERIALIZATION_ERROR = "Serialization error";
+
 }
