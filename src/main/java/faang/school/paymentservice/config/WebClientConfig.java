718da711ac76aca9b6532a7dd46b3dcce456ca63
Artem Doronin <doronintema2018@yandex.ru>
2025-04-17 18:12:13 +0500

    fix bug

--- src/main/java/faang/school/paymentservice/config/WebClientConfig.java
+++ src/main/java/faang/school/paymentservice/config/WebClientConfig.java
@@ -3,6 +3,7 @@ package faang.school.paymentservice.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -15,16 +16,23 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class WebClientConfig {
 
-    public static final int TIMEOUT = 1000;
+    @Value("${webclient.connect-timeout}")
+    private int connectTimeout;
+
+    @Value("${webclient.read-timeout}")
+    private int readTimeout;
+
+    @Value("${webclient.write-timeout}")
+    private int writeTimeout;
 
     @Bean
     public WebClient webClientWithTimeout() {
         final var tcpClient = TcpClient
                 .create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeout)
                 .doOnConnected(connection -> {
-                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new ReadTimeoutHandler(readTimeout, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(writeTimeout, TimeUnit.MILLISECONDS));
                 });
 
         return WebClient.builder()
