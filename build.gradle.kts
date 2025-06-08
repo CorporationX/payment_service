8a774804153124c663305123ad3255d39b61a041
Nazar <kateryna@Katerynas-MacBook-Pro.local>
2025-06-09 00:19:03 +0200

    add rate fetcher with base currency EUR

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
      * Spring boot starters
      */
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation ("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-consul-config")
--- src/main/java/faang/school/paymentservice/PaymentApplication.java
+++ src/main/java/faang/school/paymentservice/PaymentApplication.java
@@ -2,9 +2,12 @@ package faang.school.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableRetry
+@EnableScheduling
 @EnableFeignClients("faang.school.paymentservice.client")
 public class PaymentApplication {
     public static void main(String[] args) {
