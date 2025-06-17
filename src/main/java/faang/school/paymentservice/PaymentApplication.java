088523a9ff2c8fd02882e08a7367b549e87d7f67
Nazar <kateryna@Katerynas-MacBook-Pro.local>
2025-06-17 17:17:22 +0200

    small fixes

--- src/main/java/faang/school/paymentservice/PaymentApplication.java
+++ src/main/java/faang/school/paymentservice/PaymentApplication.java
@@ -2,6 +2,7 @@ package faang.school.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
