88a7cff5e6b39131ad9a71898dda2a056e38c4b0
mrnght <misterday116@gmail.com>
2025-08-23 19:27:01 +0800

    BJS2-84103 Добавил кэширование курса валют в редис

---
+++ src/main/java/faang/school/paymentservice/dto/Rates.java
@@ -0,0 +1,16 @@
+package faang.school.paymentservice.dto;
+
+import lombok.ToString;
+
+@ToString
+public class Rates {
+    public double USD;
+    public double AUD;
+    public double CAD;
+    public double PLN;
+    public double MXN;
+    public double JPY;
+    public double KRW;
+    public double RUB;
+    public double CNY;
+}
--- src/main/java/faang/school/paymentservice/scheduler/CurrencyRateFetcher.java
+++ src/main/java/faang/school/paymentservice/scheduler/CurrencyRateFetcher.java
@@ -1,25 +1,30 @@
 package faang.school.paymentservice.scheduler;
 
+import faang.school.paymentservice.dto.Rates;
+import faang.school.paymentservice.redis.RedisCacheService;
 import faang.school.paymentservice.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CurrencyRateFetcher {
-
+    private final RedisCacheService cacheService;
     private final CurrencyService service;
-    private HashMap<String, Double> actualRates;
 
-    @Scheduled(cron = "${currency.rates.cron}")
-    @Retr
-    public void ratesCron() {
-        service.getActualRates()
-                .subscribe(rates -> actualRates.putAll(rates));
+    @Scheduled(fixedRate = 10000)
+    private void ratesCron() {
+        service.getRatesApiResponse()
+                .subscribe(responseRates -> {
+                    log.info("Курсы валют: {}", getActualRates());
+                    cacheService.cacheRates("currency_rates", responseRates.rates);
+                });
+    }
+
+    public Rates getActualRates() {
+        return cacheService.getCachedRates("currency_rates");
     }
 }
