f55c23fc17ba72a38659b0c90925c3851863886b
JekaCAP <agentgeka@yandex.ru>
2025-08-31 15:03:20 +0500

    BJS2-84163 Убраны комментарии и лишняя аннотация.

--- src/main/java/faang/school/paymentservice/repository/PaymentRepository.java
+++ src/main/java/faang/school/paymentservice/repository/PaymentRepository.java
@@ -4,14 +4,12 @@ import faang.school.paymentservice.model.Payment;
 import faang.school.paymentservice.model.enums.PaymentStages;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Optional<Payment> findByIdempotencyToken(UUID idempotencyToken);
