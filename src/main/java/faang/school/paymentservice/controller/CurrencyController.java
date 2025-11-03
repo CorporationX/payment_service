32a8c01b0b19701e818478178ec90058302e8ccf
ayazs <ayaz-22@yandex.ru>
2025-11-04 01:22:06 +0500

    BJS2-96826 done

--- src/main/java/faang/school/paymentservice/controller/CurrencyController.java
+++ src/main/java/faang/school/paymentservice/controller/CurrencyController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/currency-rate")
+@RequestMapping("api/v1/currency-rates")
 @RequiredArgsConstructor
 public class CurrencyController {
 
