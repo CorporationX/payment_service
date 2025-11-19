87c5aaf61d1f625b86b498f280dfa9ca67e6511c
Mikhail Easy <mghievrie@bk.ru>
2025-11-20 04:42:28 +0500

    BJS2-96841 minor fix

--- src/main/java/faang/school/paymentservice/schedule/ScheduledCurrencyRateFetcher.java
+++ src/main/java/faang/school/paymentservice/schedule/ScheduledCurrencyRateFetcher.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class ScheduledCurrencyRateFetcher {
     private final CurrencyService currencyService;
     @Value("${current-rate.refresh-after-write}")
